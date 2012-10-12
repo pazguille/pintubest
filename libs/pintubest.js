2a34c4acfc35b35f85f0f268af2822b6609bdc9f
Guillermo Paz <guille87paz@gmail.com>
2012-10-12 02:45:59 +0000

    Pintubest lib: Change most_popular to recently_featured

--- libs/pintubest.js
+++ libs/pintubest.js
@@ -45,7 +45,7 @@ var sys = require("util"),
 				options = function () {
 					return {
 						host: "gdata.youtube.com",
-						path: "/feeds/api/standardfeeds/most_popular?v=2&time=today&alt=json&format=5&max-results=" + max_results + "&start-index=" + (max_results * start - max_results + 1),
+						path: "/feeds/api/standardfeeds/recently_featured?v=2&time=today&alt=json&format=5&max-results=" + max_results + "&start-index=" + (max_results * start - max_results + 1),
 						method: "GET"
 					}
 				};
